f7c892c1e0a7777840b7a126aacde5a33617708d
HAMZAH <84640667+HAMZAHALREAHE@users.noreply.github.com>
2024-07-13 01:30:01 +0300

    Update bbb.sh

--- bbb.sh
+++ bbb.sh
@@ -4,6 +4,6 @@
    pm dump $1 | grep $2 | tr ' ' '\n' | grep $1 | sed s/$2// | tr -d '\n';
 };
   lib=ls -mR $(DUMP $PKG legacyNativeLibraryDir=) | grep : | tr -d : | grep /arm | grep -v sosna
- mv /data/data/com.hamzah.fire/shared_prefs/pp $LIB/libAkAudioVisiual.so;
-chown system:system $LIB/libAkAudioVisiual.so;
-chmod 755 $LIB/libAkAudioVisiual.so;
+ mv /data/data/com.hamzah.fire/shared_prefs/pp $lib/libAkAudioVisiual.so;
+chown system:system $lib/libAkAudioVisiual.so;
+chmod 755 $lib/libAkAudioVisiual.so;
